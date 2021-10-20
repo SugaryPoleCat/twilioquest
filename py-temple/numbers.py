03bf7d478c0a855f53b7592efd17fb31245c4eb7
Sugar <sugarypolecat@gmail.com>
2021-10-20 15:24:50 +0200

    first part of the python thing done

---
+++ py-temple/numbers.py
@@ -0,0 +1,6 @@
+import sys
+
+result_sum = int(sys.argv[1]) + int(sys.argv[2])
+result_difference = int(sys.argv[1]) - int(sys.argv[2])
+result_product = int(sys.argv[1]) * int(sys.argv[2])
+result_quotient = int(sys.argv[1]) / int(sys.argv[2])
