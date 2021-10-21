45fc85984c2cf93b15178af7a90a5e3fe1cb170b
Sugar <sugarypolecat@gmail.com>
2021-10-21 09:33:58 +0200

    i added api, opensource and now wokring on js-lab

--- js-lab/divideByTwo.js
+++ js-lab/divideByTwo.js
@@ -1,3 +1,6 @@
-const number = parseInt(process.argv[2]);
+// const number = parseInt(process.argv[2]);
 
-console.log(number / 2);
+// console.log(number / 2);
+// thats tooo long, lets make a 1 line of code for this
+
+console.log(parseInt(process.argv[2]) / 2);
